5ef99cbce61e6587ab9614382eed6ebf9a608417
ysnows <ysnows@163.com>
2025-01-14 22:03:22 +0800

    first

---
+++ src/api_list_servers.ts
@@ -0,0 +1,12 @@
+import { RequestOptions, EnconvoResponse, MCP } from '@enconvo/api'
+export default async function main(req: Request): Promise<EnconvoResponse> {
+
+    let options: RequestOptions = await req.json()
+    const servers = await MCP.listServers()
+
+
+    return JSON.stringify(servers, null, 2)
+}
+
+
+
